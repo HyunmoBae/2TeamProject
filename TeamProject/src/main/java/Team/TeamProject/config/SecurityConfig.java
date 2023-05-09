23094ac220a040a06b61853610df4c50d77544fa
Parkdolodong <qufehdwls@naver.com>
2023-05-09 17:33:24 +0900

    entity 작성 (Member, MemberAddress, Board, Image) spring security 설정 주석처리

---
+++ TeamProject/src/main/java/Team/TeamProject/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+//package Team.TeamProject.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.formLogin()
+//                .loginPage("/sign/sign-in")
+//                .defaultSuccessUrl("/")
+//                .usernameParameter("id")
+//                .passwordParameter("password")
+//                .failureUrl("/sign/sign-in/error")
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/sign/logout"))
+//                .logoutSuccessUrl("/")
+//                .and();
+//
+//        http.authorizeRequests()
+//                .mvcMatchers("/css/**", "/js/**", "/img/**", "/assets/**").permitAll()
+//                .mvcMatchers("/", "/fragments/**", "/sign/**").permitAll()
+//                .mvcMatchers("/item/**").hasAnyRole("ADMIN", "USER")
+//                .anyRequest().authenticated();
+//
+//        http.exceptionHandling()
+//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+//
+//        http.csrf().disable().cors().disable();
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
---
+++ TeamProject/src/main/java/Team/TeamProject/entity/Image.java
@@ -0,0 +1,26 @@
+package Team.TeamProject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    private String imgName; // 이미지 원본 이름
+    private String imgPath; // 이미지 파일 경로
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_idx")
+    private Board board;
+}
