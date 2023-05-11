c9ab91648b6aa923c2c16f8bc92f178dd1ad6882
HyunmoBae <80663476+HyunmoBae@users.noreply.github.com>
2023-05-11 14:31:45 +0900

    Update Map.java

--- TeamProject/src/main/java/Team/TeamProject/entity/Map.java
+++ TeamProject/src/main/java/Team/TeamProject/entity/Map.java
@@ -20,20 +20,20 @@ public class Map {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long map_idx;
 
-    String opnSfTeamCode;
-    String updateGbn;
-    String updateDt;
-    String opnSvcNm;
-    String uptaeNm;
-    String bplcNm;
-    String siteWhlAddr;
-    String rdnPostNo;
-    String rdnWhlAddr;
-    String apvPermYmd;
-    String apvCancelYmd;
-    String dcbYmd;
-    String dtlStateNm;
-    String x;
-    String y;
-    String siteTel;
+    String opnSfTeamCode; //개방자치단체코드
+    String updateGbn; //데이터 갱신구분
+    String updateDt; //데이터 갱신일자
+    String opnSvcNm; //개방서비스 명
+    String uptaeNm; //업태구분명 
+    String bplcNm; //사업장 명 
+    String siteWhlAddr; //지번주소
+    String rdnPostNo; //도로명 우편번호
+    String rdnWhlAddr; //도로명 주소
+    String apvPermYmd; //인허가 일자
+    String apvCancelYmd; //인허가 취소일자
+    String dcbYmd; //폐업일자
+    String dtlStateNm; //상세영업상태 
+    String x; //x좌표
+    String y; //y좌표
+    String siteTel; //전화번호
 }
